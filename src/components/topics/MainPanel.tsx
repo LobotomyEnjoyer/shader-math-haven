6062e433826ca5db60a1e563f7d44507dd7c9e01
[blank] <76683471+LobotomyEnjoyer@users.noreply.github.com>
2025-04-25 17:18:25 +0400

    -

--- src/components/topics/MainPanel.tsx
+++ src/components/topics/MainPanel.tsx
@@ -95,7 +95,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
 
             
             <img
-            src = {"@/src/imgs/CMYK_after_opti.png"} 
+            src = {"@/imgs/CMYK_after_opti.png"} 
             alt="CMYK"
             className="w-full max-w-2xl h-auto rounded-lg shadow-md my-4"></img>
               
